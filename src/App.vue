b27984dac2a8249cdbc8a3e495c0b366a0b3bacc
Greg T Robertson <gregtrobertson@gmail.com>
2017-12-06 20:21:29 -0500

    Initial check in with working nav

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <v-app light>
+  <v-toolbar>
+    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+    <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat
+                v-for="menuItem in menuItems"
+                :key="menuItem.title"
+                router :to="menuItem.link">{{ menuItem.title }}
+                <v-icon left dark>{{ menuItem.icon }}</v-icon>
+            </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    },
+    computed: {
+      menuItems () {
+        let menuItems = [
+          {
+            icon: 'play_circle_outline', title: 'Start Game', link: '/start'
+          },
+          {
+            icon: 'group_add', title: 'Add User', link: '/adduser'
+          }
+
+        ]
+
+        return menuItems
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+
+</style>
